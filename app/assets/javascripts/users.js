18db51d0e07ae08dbd315f61d7516bf135925a5f
Ian <iantskon@gmail.com>
2015-01-21 23:33:57 -0800

    Add JS and stage for heroku

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,9 @@
+// <script type="text/javascript">
+function updateInput() {
+	var elem = document.getElementById("new_user");
+	elem.value = "My default value";
+}
+// </script>
 function submitAttendance() {
   var frm = document.getElementById('post_form');
   if (frm) {
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
 	def index
+		@users = User.all
+	end
+
+	def create
+		@user = User.create(user_params)
+		redirect_to users_path
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name)
 	end
 end
